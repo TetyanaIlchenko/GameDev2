6652fb1ffc67656370d142d85e1e2c9da69b3983
TetyanaIlchenko <tetyanailchenko64@gmail.com>
2018-06-04 13:18:03 +0300

    First task

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public float offset = 40f;
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+}
+
